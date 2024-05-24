1d8e8255b750c3b2114e5024f97ec1edb234debd
lldan <safinildan@gmail.com>
2024-05-24 20:14:04 +0300

    52

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
 import type { Metadata } from 'next'
 
 import '@/shared/styles/common/index.scss'
+import { Suspense } from 'react'
+import { Metrika } from '@/widgets/metrics'
 
 export const metadata: Metadata = {
-    title: 'Vitame',
-    description: 'Vitame website'
+  title: 'Vitame',
+  description: 'Vitame website'
 }
 
 export default function RootLayout({
-    children
+  children
 }: Readonly<{
-    children: React.ReactNode
+  children: React.ReactNode
 }>) {
-    return (
-        <html lang='ru'>
-            <body>{children}</body>
-        </html>
-    )
+  return (
+    <html lang='ru'>
+      <body>
+        <Suspense>
+          <Metrika />
+        </Suspense>
+        {children}
+      </body>
+    </html>
+  )
 }
