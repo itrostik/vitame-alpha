b269558f27c8e7b0b58dde0f3712e250cb9ab179
itrostik <rostislavsergeev.it@gmail.com>
2024-05-24 20:08:52 +0300

    грязь

--- src/shared/UI/layouts/good/index.tsx
+++ src/shared/UI/layouts/good/index.tsx
@@ -6,7 +6,7 @@ import Header from '@/shared/UI/header'
 import Navigation from '@/widgets/navigation/UI'
 import Modal from '@/widgets/modal/UI/modal'
 import Form from '@/widgets/form/UI'
-import Product from '@/entities/modal/UI'
+import ModalProduct from '@/entities/modal/UI'
 import useNavbarStore from '@/widgets/navigation/model/store'
 import { IProduct } from '@/entities/product/model/store'
 import useFormStore from '@/widgets/form/model/store'
@@ -14,20 +14,20 @@ import Cookie from '@/widgets/cookie/UI'
 import useCookieStore from '@/widgets/cookie/model/store'
 
 const Good = ({ product }: { product: IProduct }) => {
-    const { isCookieHidden, setIsCookieHidden } = useCookieStore()
-    const { isFormHidden, setIsFormHidden } = useFormStore()
+  const { isCookieHidden, setIsCookieHidden } = useCookieStore()
+  const { isFormHidden, setIsFormHidden } = useFormStore()
 
-    return (
-        <div className={styles.wrapper}>
-            <Header />
-            <Navigation />
-            <Cookie />
-            <Modal isHidden={isFormHidden} setIsHidden={setIsFormHidden}>
-                <Form />
-            </Modal>
-            <Product product={product} />
-        </div>
-    )
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <Navigation />
+      <Cookie />
+      <Modal isHidden={isFormHidden} setIsHidden={setIsFormHidden}>
+        <Form />
+      </Modal>
+      <ModalProduct product={product} />
+    </div>
+  )
 }
 
 export default Good
